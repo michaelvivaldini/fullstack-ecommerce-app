332b1453a05dc4bd06c95246006eab04639d544b
Michael Vivaldini <60002937+michaelvivaldini@users.noreply.github.com>
2023-10-18 23:05:22 -0300

    feat: add product item and product list components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,27 +1,36 @@
+import { prismaClient } from "@/lib/prisma";
 import Image from "next/image";
-import { FunctionComponent } from "react";
 import Categories from "./components/categories";
+import ProductList from "./components/product-list";
 
-interface HomeProps {
-  
-}
- 
-const Home: FunctionComponent<HomeProps> = () => {
-  return ( 
-    <main className="p-5 flex flex-col gap-3">
-      <Image src="/banner-home-01.png"
+const Home = async () => {
+  const promotions = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
+  return (
+    <main className="flex flex-col gap-3">
+      <Image
+        src="/banner-home-01.png"
         height={0}
         width={0}
-        className="h-auto w-full"
+        className="h-auto w-full p-5"
         sizes="100vw"
         alt="Até 55% de desconto neste mês!"
       />
-      <div className="mt-8">
-      <Categories />
+      <div className="mt-8 p-5">
+        <Categories />
       </div>
-      
+
+      <div className="mt-8">
+        <ProductList products={promotions} />
+      </div>
     </main>
-   );
-}
- 
+  );
+};
+
 export default Home;
---
+++ src/components/ui/product-item.tsx
@@ -0,0 +1,55 @@
+import { ProductWithTotalPrice } from "@/helpers/product";
+import { ArrowDownIcon } from "lucide-react";
+import Image from "next/image";
+import { FunctionComponent } from "react";
+import { Badge } from "./badge";
+
+interface ProductItemProps {
+  product: ProductWithTotalPrice;
+}
+
+const ProductItem: FunctionComponent<ProductItemProps> = ({ product }) => {
+  return (
+    <div className="flex w-[156px] flex-col gap-4">
+      <div className="relative flex h-[170px] items-center justify-center rounded-lg bg-accent">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          alt={product.name}
+          style={{ objectFit: "contain" }}
+        />
+        {product.discountPercentage > 0 && (
+          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+            <ArrowDownIcon size={14} />
+            {product.discountPercentage}%
+          </Badge>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <p className="truncate text-sm">{product.name}</p>
+        <div className="flex items-center gap-2">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
+              </p>
+              <p className="text-xs line-through opacity-75">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            </>
+          ) : (
+            <p className="text-sm font-semibold">
+              R$ {Number(product.basePrice).toFixed(2)}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
