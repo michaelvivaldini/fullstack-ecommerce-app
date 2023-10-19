d0f105635a0716b8f9d2e0683651c0f1b1c5e92f
Michael Vivaldini <60002937+michaelvivaldini@users.noreply.github.com>
2023-10-18 23:09:04 -0300

    feat: add second banner home to homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,13 +22,24 @@ const Home = async () => {
         sizes="100vw"
         alt="Até 55% de desconto neste mês!"
       />
+
       <div className="mt-8 p-5">
         <Categories />
       </div>
 
       <div className="mt-8">
+        <p className="pl-5 mb-3 font-bold uppercase">Ofertas</p>
         <ProductList products={promotions} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className="h-auto w-full p-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses!"
+      />
     </main>
   );
 };
