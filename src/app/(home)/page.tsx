3c5ed89af78832ef4ba1201952ad6bc035fd92c9
Michael Vivaldini <60002937+michaelvivaldini@users.noreply.github.com>
2023-10-18 22:17:20 -0300

    feat: add categories and categoryItem components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,6 @@
-"use client"
-
 import Image from "next/image";
 import { FunctionComponent } from "react";
+import Categories from "./components/categories";
 
 interface HomeProps {
   
@@ -9,7 +8,7 @@ interface HomeProps {
  
 const Home: FunctionComponent<HomeProps> = () => {
   return ( 
-    <main className="p-5">
+    <main className="p-5 flex flex-col gap-3">
       <Image src="/banner-home-01.png"
         height={0}
         width={0}
@@ -17,6 +16,10 @@ const Home: FunctionComponent<HomeProps> = () => {
         sizes="100vw"
         alt="Até 55% de desconto neste mês!"
       />
+      <div className="mt-8">
+      <Categories />
+      </div>
+      
     </main>
    );
 }
