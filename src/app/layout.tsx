c8c738e19c8b0f8d2010f7e963cf64c4cc60a450
Michael Vivaldini <60002937+michaelvivaldini@users.noreply.github.com>
2023-10-19 20:18:22 -0300

    feat: adds cart context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/ui/footer";
 import Header from "@/components/ui/header";
 import { AuthProvider } from "@/providers/auth";
+import { CartProvider } from "@/providers/cart";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -20,11 +21,13 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
-        <div className="flex flex-col h-full">
+        <div className="flex h-full flex-col">
           <AuthProvider>
-            <Header />
-            <div className="flex-1">{children}</div>
-            <Footer />
+            <CartProvider>
+              <Header />
+              <div className="flex-1">{children}</div>
+              <Footer />
+            </CartProvider>
           </AuthProvider>
         </div>
       </body>
