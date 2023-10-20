5d7f813b99f52695d1db53bd0cd8895b3a3071f4
Michael Vivaldini <60002937+michaelvivaldini@users.noreply.github.com>
2023-10-19 23:43:26 -0300

    fix: ensure cart total calculates correctly when increasing or decreasing quantity

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -39,13 +39,13 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
 
   const subTotal = useMemo(() => {
     return products.reduce(
-      (acc, product) => acc + Number(product.basePrice),
+      (acc, product) => acc + Number(product.basePrice) * product.quantity,
       0,
     );
   }, [products]);
 
   const total = useMemo(() => {
-    return products.reduce((acc, product) => acc + product.totalPrice, 0);
+    return products.reduce((acc, product) => acc + product.totalPrice * product.quantity, 0);
   }, [products]);
 
   const totalDiscount = total - subTotal;
